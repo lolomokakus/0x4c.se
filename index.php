fa1f99beb7ed0f262ae24be8df52ffba00d62f49
Ludvig Holtze <ludvighg1999@gmail.com>
2018-06-04 21:36:03 +0200

    skapa koderna själv istället för att använda bilder

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>0x4c.se</title>
+	<link rel="icon" type="image/png" href="ikon.png">
+	<link rel="stylesheet" type="text/css" href="stilmall.css">
+</head>
+<body>
+	<div id="helheten">
+	<?php
+		include("qrlib.php");
+		
+		$repliker = file("repliker.txt");
+		$replik = $repliker[array_rand($repliker)];
+		
+		$rutkod = QRcode::svg($replik, "rutkod", false, QR_ECLEVEL_L, 500);
+		
+		echo $rutkod;
+	?>
+	</div>
+</body>
